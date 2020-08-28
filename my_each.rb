140a1187dcea2f3988eb83335b05fba22a7f0086
Mikel Bishop <mdrew8@gmail.com>
2020-08-28 02:44:15 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,15 @@
 def my_each(collection) 
-  while i 
-  
+ i = 0  
+ while i < array.length
+ i = i + 1 
+end
+end
+
+
+def hello_t(array)
+  i = 0
+ 
+  while i < array.length
+    i = i + 1
+  end
 end
